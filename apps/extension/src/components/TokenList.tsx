e11a93dfb5e4e31bc06ac9a6b0aa2fa95b26bd9b
zewei.zhang <zewei.zhang@soulwallet.io>
2024-10-31 16:55:13 +0800

    feat: set real assets data

--- apps/extension/src/components/TokenList.tsx
+++ apps/extension/src/components/TokenList.tsx
@@ -1,14 +1,24 @@
-import TokenItem, { TokenItemProps } from './TokenItem';
+import { TokenDTO } from '@/entries/side-panel/components/Assets';
+import TokenItem from './TokenItem';
+import { hexToNumber } from 'viem';
 
 interface TokenListProps {
-  data: TokenItemProps[];
+  data: TokenDTO[];
 }
 
 export default function TokenList({ data }: TokenListProps) {
   return (
     <div className="flex flex-col gap-y-2">
       {data.map((item) => (
-        <TokenItem key={item.name} {...item} />
+        <TokenItem
+          key={item.name}
+          name={item.symbol || item.name}
+          balance={hexToNumber(item.tokenBalance).toString()}
+          price={(
+            hexToNumber(item.tokenBalance) / Number(item.price)
+          ).toString()}
+          icon={item.logoURI}
+        />
       ))}
     </div>
   );
