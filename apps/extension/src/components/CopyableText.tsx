0066aef49f6b8b4f09ce72a7994157859b8c6e88
zewei.zhang <zewei.zhang@soulwallet.io>
2024-10-31 10:40:10 +0800

    fix: copy event bubbling

--- apps/extension/src/components/CopyableText.tsx
+++ apps/extension/src/components/CopyableText.tsx
@@ -1,6 +1,7 @@
 import { safeClipboard } from '@/utils/clipboard';
 import { cn } from '@/utils/shadcn/utils';
 import { Copy } from 'lucide-react';
+import { MouseEventHandler } from 'react';
 interface ICopyableTextProps {
   text: React.ReactNode;
   originalText: Nullable<string>;
@@ -12,13 +13,14 @@ export default function CopyableText({
   originalText,
   className,
 }: ICopyableTextProps) {
+  const handleClick: MouseEventHandler<SVGSVGElement> = (e) => {
+    e.stopPropagation();
+    safeClipboard(originalText || '');
+  };
   return (
     <div className={cn('flex flex-row gap-2 items-center', className)}>
       <div>{text}</div>
-      <Copy
-        className="w-4 h-4 cursor-pointer"
-        onClick={() => safeClipboard(originalText || '')}
-      />
+      <Copy className="w-4 h-4 cursor-pointer" onClick={handleClick} />
     </div>
   );
 }
