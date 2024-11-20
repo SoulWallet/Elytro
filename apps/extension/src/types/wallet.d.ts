0373a09d1ee8a58fda6f5d67f3f3daecff7324c7
joi.y.z <joi.yue.zou@gmail.com>
2024-11-20 11:27:03 +0800

    chore: remove unused code

--- apps/extension/src/types/wallet.d.ts
+++ apps/extension/src/types/wallet.d.ts
@@ -70,26 +70,3 @@ type TApprovalInfo = {
   reject: (data?: unknown) => void;
   winId?: number;
 };
-
-type WaitForUserOperationTxParameters = {
-  hash: Hex;
-  /**
-   * Exponential backoff paramters that can be used to override
-   * the configuration on the client. If not provided, this method
-   * will use the paramters passed via the `opts` parameter on the
-   * smart account client.
-   */
-  retries?: {
-    /**
-     * the base retry interval or delay between requests
-     */
-    intervalMs: number;
-    /**
-     * the multiplier to exponentiate based on the number retries
-     * setting this to one will result in a linear backoff
-     */
-    multiplier: number;
-    /** the maximum number of retries before failing */
-    maxRetries: number;
-  };
-};
