b7099b09a2aca1894537f2f236c3f2cd54b25d04
zewei.zhang <zewei.zhang@soulwallet.io>
2024-12-17 14:40:05 +0800

    feat: change font size follow the value length

--- apps/extension/src/entries/side-panel/components/AmountInput.tsx
+++ apps/extension/src/entries/side-panel/components/AmountInput.tsx
@@ -1,6 +1,8 @@
 import { Input } from '@/components/ui/input';
 import { ArrowRightLeftIcon } from 'lucide-react';
 import { FieldValues } from 'react-hook-form';
+import { cn } from '@/utils/shadcn/utils';
+import { useMemo } from 'react';
 
 export default function AmountInput({
   field,
@@ -11,13 +13,17 @@ export default function AmountInput({
   isDisabled: boolean;
   price: string | number;
 }) {
+  const dynamicFontSize = useMemo(
+    () => (field.value?.length > 10 ? 'text-xl' : 'text-5xl'),
+    [field.value]
+  );
   return (
     <div className="bg-white px-3 py-4 rounded-md flex flex-row items-center">
       <Input
-        className="text-5xl border-none"
+        {...field}
+        className={cn('border-none', dynamicFontSize)}
         placeholder="0"
         disabled={isDisabled}
-        {...field}
       />
       <div className="bg-gray-300 p-2 rounded-sm">
         <ArrowRightLeftIcon className="w-4 h-4" />
