f5b309cc1df4f8f867581e1a3d8e30c6692d3987
zewei.zhang <zewei.zhang@soulwallet.io>
2024-12-19 11:33:15 +0800

    refactor: create new address with chain item comp

--- apps/extension/src/entries/side-panel/pages/CreateNewAddress.tsx
+++ apps/extension/src/entries/side-panel/pages/CreateNewAddress.tsx
@@ -1,35 +1,44 @@
 import { Button } from '@/components/ui/button';
 import { useChain } from '../contexts/chain-context';
 import SecondaryPageWrapper from '../components/SecondaryPageWrapper';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-} from '@/components/ui/select';
 import { useState } from 'react';
 import ChainItem from '@/components/ChainItem';
+import { TChainConfigItem } from '@/constants/chains';
+import { toast } from '@/hooks/use-toast';
 
 export default function CreateNewAddress() {
-  const { chains } = useChain();
-  const [selectedChain, setSelectedChain] = useState(chains[0]);
+  const { chains, currentChain } = useChain();
+  const [selectedChain, setSelectedChain] = useState<TChainConfigItem | null>(
+    null
+  );
   const handleChange = (value: string) => {
     setSelectedChain(
-      chains.find((chain) => chain.chainId.toString() === value) || chains[0]
+      () =>
+        chains.find((chain) => chain.chainId.toString() === value) || chains[0]
     );
+  };
+
+  const handleCreate = () => {
+    if (!selectedChain) return;
+    if (currentChain?.chainId === selectedChain.chainId) {
+      toast({
+        title: 'Create address failed',
+        description: 'You are already on this network',
+      });
+    }
   };
   return (
     <SecondaryPageWrapper
       title="Create New Address"
       footer={
-        <div className="flex justify-between w-full gap-4">
-          <Button variant="outline" size="large" className="w-full">
-            Cancel
-          </Button>
-          <Button size="large" className="w-full">
-            Continue
-          </Button>
-        </div>
+        <Button
+          size="large"
+          className="w-full"
+          onClick={handleCreate}
+          disabled={!selectedChain}
+        >
+          Create
+        </Button>
       }
     >
       <div className=" text-lg font-semibold mb-2">Create a new address</div>
@@ -37,25 +46,18 @@ export default function CreateNewAddress() {
         You are about to create a new address within the below network. You can
         use the same local password to access it.
       </div>
-      <Select onValueChange={handleChange}>
-        <SelectTrigger className="h-16 px-5 py-0 rounded-sm">
-          <ChainItem chain={selectedChain} />
-        </SelectTrigger>
-        <SelectContent className="rounded-sm bg-white overflow-hidden w-full">
-          {chains.map((chain) => {
-            return (
-              <SelectItem
-                key={chain.chainId}
-                needCheckIcon={false}
-                value={chain.chainId.toString()}
-                className="px-5 py-0 h-16 rounded-none"
-              >
-                <ChainItem chain={chain} />
-              </SelectItem>
-            );
-          })}
-        </SelectContent>
-      </Select>
+      <div className="space-y-2">
+        {chains.map((chain) => {
+          return (
+            <ChainItem
+              isSelected={selectedChain?.chainId === chain.chainId}
+              key={chain.chainId}
+              chain={chain}
+              onClick={() => handleChange(chain.chainId.toString())}
+            />
+          );
+        })}
+      </div>
     </SecondaryPageWrapper>
   );
 }
